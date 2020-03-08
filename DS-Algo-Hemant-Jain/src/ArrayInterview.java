5e1d399701049d0aa10524914ee90f29ff016ff1
aminul <aminul@dohatec.com.bd>
2020-03-08 12:02:55 +0600

    ArrayInterview End

--- DS-Algo-Hemant-Jain/src/ArrayInterview.java
+++ DS-Algo-Hemant-Jain/src/ArrayInterview.java
@@ -19,6 +19,7 @@ public class ArrayInterview {
     }
 
     public static int BinarySearch (int arr[], int size, int value) {
+        // Binary Search Algorithm – Iterative Way
         int mid;
         int low = 0;
         int high = size-1;
@@ -35,17 +36,17 @@ public class ArrayInterview {
         return -1;
     }
 
-    public static void rotateArray(int[] a,int n,int k) {
+    public static void rotateArray(int[] a, int n, int k) {
         reverseArray(a,0,k-1);
         reverseArray(a,k,n-1);
         reverseArray(a,0,n-1);
     }
 
     public static void reverseArray(int[] a,int start, int end) {
-        for(int i=start,j=end;i<j;i++,j--) {
+        for(int i = start, j = end; i < j; i++, j--) {
             int temp = a[i];
-            a[i]=a[j];
-            a[j]=temp;
+            a[i] = a[j];
+            a[j] = temp;
         }
     }
 
@@ -62,11 +63,23 @@ public class ArrayInterview {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-        System.out.println("sum of all the values in array: " + SumArray(arr));
+//        int[] arr = {1,2,3,4,5,6,7,8,9};
+//        System.out.println("sum of all the values in array: " + SumArray(arr));
 
-//        int[] arr = {1,-2,3,4,-4,6,-14,8,2};
-//        System.out.println("Max sub array sum :" + maxSubArraySum(arr, 9));
+//        int[] arr = {1,2,3,4,5,6,7,8,9};
+//        System.out.println("Sequential Search result from Array : " + SequentialSearch(arr, arr.length, 7));
+
+//        int[] arr = {1,2,3,4,5,6,7,8,9};
+//        System.out.println("Binary Search result from Array : " + BinarySearch(arr, arr.length, 7));
+
+//        int[] arr = {1,2,3,4,5,6,7,8,9};
+//        rotateArray(arr, arr.length, 4);
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i] + " ");
+//        }
+
+        int[] arr = {1,-2,3,4,-4,6,-14,8,2};
+        System.out.println("Max sub array sum : " + maxSubArraySum(arr, 9));
     }
 
 }
